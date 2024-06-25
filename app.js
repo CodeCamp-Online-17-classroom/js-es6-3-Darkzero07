2da9eb4475c7b2af3d6e0e97cdcadc02a35e3b0b
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-26 00:13:50 +0700

    js lab 147

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// write code here
+// จงเขียนฟังก์ชัน mergeObjects โดยพารามิเตอร์ของฟังก์ชันสามารถรับ Object ได้ไม่จำกัด และให้ return ค่าเป็น Object ที่เกิดจาก Object ในพารามิเตอร์มา merge กัน (ในกรณีที่ Object ในพารามิเตอร์มี key ซ้ำกันให้ยึดค่าพารามิเตอร์ตัวที่มาก่อนเป็นหลัก)
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { b: 3, c: 4 };
+const obj3 = { d: 5 };
+
+function mergeObjects(...objects) {
+  return objects.reduce((acc, obj) => {
+    return { ...acc, ...obj };
+  }, {});
+}
+
+console.log(mergeObjects(obj1, obj2, obj3));
+// { a: 1, b: 3, c: 4, d: 5 }
